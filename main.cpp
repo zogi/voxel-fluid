1cb7f23e0b19112c2cf6ae6d5f62b884d069e092
Zoltan Gilian <zoltan.gilian@gmail.com>
2018-01-07 10:32:59 +0000

    first working 3D DDA voxel traversal

--- main.cpp
+++ main.cpp
@@ -272,29 +272,68 @@ static std::string octree_renderer_fs_code = common_shader_code + R"glsl(
         return (rand2(uv, time) + rand2(12.3456*uv, 76.5432*time) - 0.5) / 255.0;
     }
 
-    in vec2 uv;
-    void main() {
-        vec3 ray_dir_view = vec3(view_size * (uv - 0.5), -1.0);
-        vec3 ray_dir_world = normalize(eye_orientation * ray_dir_view);
+    struct Ray {
+        vec3 origin;
+        vec3 direction;
+    };
 
-        // Trace a single cube for now.
-        vec3 origin = vec3(0, 0, 0);
-        vec3 size = vec3(1, 1, 1);
+    vec3 getRayPos(Ray ray, float t)
+    {
+        return ray.origin + t * ray.direction;
+    }
 
-        // Compute entry and exit point parameters.
-        vec3 t0 = (origin - eye_pos) / ray_dir_world;
-        vec3 t1 = (origin + size - eye_pos) / ray_dir_world;
+    struct Box {
+        vec3 origin;
+        vec3 size;
+    };
+
+    // Returns the parameters when the ray enters and exits the box in
+    // the first and second component of a vec2 respectively.
+    // If the enter parameter is greater than the exit parameter, the ray
+    // misses the box.
+    vec2 intersectRayBox(Ray ray, Box box)
+    {
+        vec3 t0 = (box.origin - ray.origin) / ray.direction;
+        vec3 t1 = (box.origin + box.size - ray.origin) / ray.direction;
         vec3 tm = min(t0, t1);
         vec3 tM = max(t0, t1);
-        float t_in =  max(tm.x, max(tm.y, tm.z));
-        float t_out = min(tM.x, min(tM.y, tM.z));
+        float t_enter = max(tm.x, max(tm.y, tm.z));
+        float t_exit = min(tM.x, min(tM.y, tM.z));
+        return vec2(t_enter, t_exit);
+    }
+
+    vec3 getVoxelExtinction(ivec3 index)
+    {
+        if (length(vec3(index) - vec3(1.5, 1.5, 1.5)) < 2)
+            return vec3(4, 8, 4);
+        else
+            return vec3(0, 0, 0);
+    }
+
+    in vec2 uv;
+    void main() {
+        Ray ray;
+        ray.origin = eye_pos;
+        vec3 ray_dir_view = vec3(view_size * (uv - 0.5), -1.0);
+        ray.direction = normalize(eye_orientation * ray_dir_view);
+
+        // Find the entry and exit points of the octree domain.
+        Box bounds;
+        bounds.origin = vec3(0, 0, 0);
+        bounds.size = vec3(1, 1, 1);
+
+        // Compute entry and exit point parameters.
+        vec2 params = intersectRayBox(ray, bounds);
+        float t_in = params.x;
+        float t_out = params.y;
 
         // Discard if ray misses the octee bounds.
-        if (t_in >= t_out || t_out < 0)
+        if (t_in >= t_out || t_out < 0) {
             discard;
+        }
 
         // Handle camera inside octree bounds.
-        float t_near = cam_nearz / dot(eye_dir, ray_dir_world);
+        float t_near = cam_nearz / dot(eye_dir, ray.direction);
         if (t_in < 0 && t_out > 0) {
             t_in = t_near;
         }
@@ -305,10 +344,45 @@ static std::string octree_renderer_fs_code = common_shader_code + R"glsl(
         t_in = min(t_in, t_thresh);
         t_out = min(t_out, t_thresh);
 
-        // Compute transmittance.
-        float optical_depth = t_out - t_in;
-        vec3 extinction = vec3(4, 4, 4);
-        vec3 transmittance = exp(-extinction * optical_depth);
+        // Perform 3D DDA traversal.
+        float eps = 1e-5;
+        ivec3 index_min = ivec3(0, 0, 0);
+        ivec3 index_max = ivec3(4, 4, 4);
+        ivec3 index_entry = ivec3(floor((getRayPos(ray, t_in + eps) - bounds.origin) / bounds.size * index_max));
+        ivec3 index = index_entry;
+        Box voxel;
+        voxel.size = bounds.size / index_max;
+        voxel.origin = index_entry * voxel.size;
+        vec3 transmittance = vec3(1, 1, 1);
+        float t = t_in;
+        ivec3 step = ivec3(sign(ray.direction));
+        for (int i = 0; i < 10; ++i) {
+            vec3 extinction = getVoxelExtinction(index);
+            vec3 t0 = (voxel.origin - ray.origin) / ray.direction;
+            vec3 t1 = (voxel.origin + voxel.size - ray.origin) / ray.direction;
+            vec3 tM = max(t0, t1);
+            float t_exit = min(tM.x, min(tM.y, tM.z));
+            t_exit = min(t_exit, t_out);
+            transmittance *= exp(-extinction * (t_exit - t));
+            t = t_exit;
+            if (t >= t_out)
+                break;
+            if (t_exit == tM.x) {
+                index.x += step.x;
+                voxel.origin.x += step.x * voxel.size.x;
+            }
+            if (t_exit == tM.y) {
+                index.y += step.y;
+                voxel.origin.y += step.y * voxel.size.y;
+            }
+            if (t_exit == tM.z) {
+                index.z += step.z;
+                voxel.origin.z += step.z * voxel.size.z;
+            }
+            if (any(lessThan(index, index_min)) || any(greaterThanEqual(index, index_max))) {
+                break;
+            }
+        }
 
         // Dither.
         if (dithering_enabled) {
