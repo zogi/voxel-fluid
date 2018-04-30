aa1c22d5d612d100ad85bc9f7420cae0b61d2d4e
Zoltan Gilian <zoltan.gilian@gmail.com>
2018-04-30 10:15:47 +0100

    add tabs to settings GUI

--- main.cpp
+++ main.cpp
@@ -1176,55 +1176,15 @@ static void ShowSettings(bool *p_open)
     }
 
     ImGui::PushStyleVar(ImGuiStyleVar_FramePadding, ImVec2(2, 2));
-    ImGui::Columns(2);
-    ImGui::Separator();
 
-    struct funcs {
-        static void ShowDummyObject(const char *prefix, int uid)
-        {
-            ImGui::PushID(uid); // Use object uid as identifier. Most commonly you could also use
-                                // the object pointer as a base ID.
-            ImGui::AlignTextToFramePadding(); // Text and Tree nodes are less high than regular
-                                              // widgets, here we add vertical spacing to make the
-                                              // tree lines equal high.
-            bool node_open = ImGui::TreeNode("Object", "%s_%u", prefix, uid);
-            ImGui::NextColumn();
-            ImGui::AlignTextToFramePadding();
-            ImGui::Text("my sailor is rich");
-            ImGui::NextColumn();
-            if (node_open) {
-                static float dummy_members[8] = { 0.0f, 0.0f, 1.0f, 3.1416f, 100.0f, 999.0f };
-                for (int i = 0; i < 8; i++) {
-                    ImGui::PushID(i); // Use field index as identifier.
-                    if (i < 2) {
-                        ShowDummyObject("Child", 424242);
-                    } else {
-                        ImGui::AlignTextToFramePadding();
-                        // Here we use a Selectable (instead of Text) to highlight on hover
-                        // ImGui::Text("Field_%d", i);
-                        char label[32];
-                        sprintf(label, "Field_%d", i);
-                        ImGui::Bullet();
-                        ImGui::Selectable(label);
-                        ImGui::NextColumn();
-                        ImGui::PushItemWidth(-1);
-                        if (i >= 5)
-                            ImGui::InputFloat("##value", &dummy_members[i], 1.0f);
-                        else
-                            ImGui::DragFloat("##value", &dummy_members[i], 0.01f);
-                        ImGui::PopItemWidth();
-                        ImGui::NextColumn();
-                    }
-                    ImGui::PopID();
-                }
-                ImGui::TreePop();
-            }
-            ImGui::PopID();
-        }
-    };
+    ImGui::BeginTabBar("Settings#main_tab_bar");
+    ImGui::DrawTabsBackground();
 
     // Rendering settings.
-    {
+    if (ImGui::AddTab("Rendering")) {
+        ImGui::Columns(2);
+        ImGui::Separator();
+
         ImGui::PushID(0);
         ImGui::AlignTextToFramePadding();
         ImGui::SetNextTreeNodeOpen(true, ImGuiCond_FirstUseEver);
@@ -1284,7 +1244,10 @@ static void ShowSettings(bool *p_open)
     }
 
     // Simulation settings.
-    {
+    if (ImGui::AddTab("Fluid simulation")) {
+        ImGui::Columns(2);
+        ImGui::Separator();
+
         ImGui::PushID(1);
         ImGui::AlignTextToFramePadding();
         ImGui::SetNextTreeNodeOpen(true, ImGuiCond_FirstUseEver);
@@ -1326,6 +1289,8 @@ static void ShowSettings(bool *p_open)
         }
         ImGui::PopID();
     }
+
+    ImGui::EndTabBar();
 
     ImGui::Columns(1);
     ImGui::Separator();
