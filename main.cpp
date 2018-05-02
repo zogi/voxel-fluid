8017f23f75b22a03b34c692aaa578a0acc1984ad
Zoltan Gilian <zoltan.gilian@gmail.com>
2018-05-02 20:37:21 +0100

    set OpenGL version to 4.5
    glTextureSubImage3D is core since 4.5.

--- main.cpp
+++ main.cpp
@@ -1374,7 +1374,7 @@ int main()
 
     // Create window.
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 5);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
     glfwWindowHint(GLFW_DEPTH_BITS, 0);
     glfwWindowHint(GLFW_STENCIL_BITS, 0);
